1084e45b73732bdd848f0a30ccc35ea627b8b874
Dino Gržinić <grzinicdino@gmail.com>
2021-04-12 16:56:47 +0200

    Update ico converter.py

--- ico converter.py
+++ ico converter.py
@@ -4,13 +4,11 @@ def ico_convert(filename):
     from PIL import Image
     img=Image.open(filename)
     filename=filename.replace(filename[len(filename)-4:],'.ico')
-    print(filename)
     img.save(f'{filename}', format='ico')
 
 
 file_list=[]
 ico='.ico'
-png='.png'
 this_file='ico converter.py'
 
 for root, dirs, files in os.walk("."):
@@ -22,12 +20,12 @@ br=0
 convert=[]
 
 for i in file_list:
-    file=file_list[i]
-    file=file[:len(file)-4]
-    if (file+ico) not in file_list:
-        ico_convert(file+ico)
+    file=i
+    i=i[:len(i)-4]
+    if (i+ico) not in file_list:
+        ico_convert(file)
         br+=1
-        convert.append(file+png)
+        convert.append(file)
 
 if br==0:
     print('no files have been converted')
