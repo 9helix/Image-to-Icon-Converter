cb23a9b275cb19ec934f8700b4395ead05f1b661
Dino Gržinić <grzinicdino@gmail.com>
2021-05-20 19:45:55 +0200

    Add files via upload

--- ico converter.py
+++ ico converter.py
@@ -1,5 +1,6 @@
 import os
 from PIL import Image
+import webbrowser
 
 def ico_convert(filename):
     img=Image.open(png_path+filename)
@@ -9,10 +10,11 @@ def ico_convert(filename):
 png_list=[]
 ico_list=[]
 ico='.ico'
+png='.png'
 this_file='png to ico.py'
 dir_path = os.path.dirname(os.path.realpath(__file__))
 png_path = dir_path+'\PNG/'
-ico_path=dir_path+"\ICO/"
+ico_path = dir_path+"\ICO/"
 
 for entry in os.listdir(png_path):
     if os.path.isfile(os.path.join(png_path, entry)):
@@ -25,22 +27,30 @@ for entry in os.listdir(ico_path):
         ico_list.append(entry)           
 
 br=0
+converted=[]
 
 for file in png_list:
     if file in ico_list:
-        png_list.remove(file)
+        None
     if file not in ico_list:
-        ico_convert(file+'.png')
+        filename=file+png
+        ico_convert(rf'{filename}')
         br+=1
+        converted.append(file)
 
+f=open('log.txt','w')
 if br==0:
     print('No files have been converted.')
+    f.write('No files have been converted.')
 
-else:
-    print('{len(convertt)} files have been converted:')
-    for i in range (len(png_list)):
-        print(png_list[i]+'.png')
-
+else:    
+    print(f'{len(converted)} files have been converted:')
+    f.write(f'{len(converted)} files have been converted:')
+    for i in range (len(converted)):
+        print(converted[i]+'.png')
+        f.write(converted[i]+'.png')
+f.close()
+webbrowser.open('log.txt')
 
 
         
